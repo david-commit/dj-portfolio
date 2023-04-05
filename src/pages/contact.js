ad42a8b19d840acb291589b985de83e079016617
david-commit <davidondiege@gmail.com>
2023-04-05 10:42:56 +0300

    Added contact route and component

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from 'next/head';
+
+const contact = () => {
+  return (
+    <div>
+      <Head>
+        <title>Conatct - Dj Prince Kym</title>
+      </Head>
+      <main>CONTACT Page</main>
+    </div>
+  );
+};
+
+export default contact;
