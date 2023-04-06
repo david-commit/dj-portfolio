7969aa715dbed868fe38b423813b31c0408a28a3
david-commit <davidondiege@gmail.com>
2023-04-06 13:33:05 +0300

    Commented out dark theme for development

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home - Dj Prince Kym</title>
-        <meta name='description' content='Dj Prince Kym is a widely known gospel deejay and videographer' />
+        <meta
+          name='description'
+          content='Dj Prince Kym is a widely known gospel deejay and videographer'
+        />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
-      <main>
-        HOME
-      </main>
+      <div>HOME</div>
     </>
   );
 }
