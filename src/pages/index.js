c7155a9a55825ca8d9b33d977f753f2b97a676e4
david-commit <davidondiege@gmail.com>
2023-04-06 23:49:04 +0300

    Created an image slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,41 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import { useRef } from 'react';
 
 export default function Home() {
+  const [index, setIndex] = useState(0);
+  const timeoutRef = useRef(null);
+  const colors = ['#0088FE', '#00C49F', '#FFBB28'];
+  const sliderImages = [
+    '/slider-images/dj-p-k-nrg-radio.jpg',
+    '/slider-images/dj-p-k-dark-home-banner.jpg',
+    '/slider-images/dj-p-k-kubash-event.jpg',
+    '/slider-images/dj-p-k-garden.jpg',
+  ];
+  const delay = 5000;
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === sliderImages.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
   return (
     <>
       <Head>
@@ -11,7 +46,39 @@ export default function Home() {
         />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
-      <div>HOME</div>
+      <div className='home-main-container'>
+        <div className='home-slider'>
+          <div className='slideshow'>
+            <div
+              className='slideshowSlider'
+              style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+            >
+              {sliderImages.map((image, index) => (
+                <img
+                  className='slide'
+                  key={index}
+                  src={image}
+                  alt='Slider Image'
+                  // height={100}
+                  // width={100}
+                />
+              ))}
+            </div>
+
+            <div className='slideshowDots'>
+              {sliderImages.map((_, idx) => (
+                <div
+                  key={idx}
+                  className={`slideshowDot${index === idx ? ' active' : ''}`}
+                  onClick={() => {
+                    setIndex(idx);
+                  }}
+                ></div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
