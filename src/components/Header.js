b98e96e2f9949b161fc77dac467f605d4e9a9c71
david-commit <davidondiege@gmail.com>
2023-04-06 13:31:27 +0300

    Created header and imported font

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React from 'react';
+import logo from '../images/PRINCE-KIM-LOGO.png';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <header>
-     <a href="/">
-      <img src="" alt="" />
-     </a>
+      <section>
+        <Link href='/'>
+          <Image src={logo} alt='' priority />
+        </Link>
+        <nav>
+          <Link href='/'>Home</Link>
+          <Link href='/about'>About Me</Link>
+          <Link href='/services'>Services</Link>
+          <Link href='/hire'>Hire</Link>
+          <Link href='/mixes'>Mixes</Link>
+          <Link href='/contacts'>Contacts</Link>
+        </nav>
+      </section>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import Header from '@/components/Header';
+import '../styles/Header.css';
+import { Open_Sans } from 'next/font/google'
+
+const openSans = Open_Sans({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700']
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={openSans.className}>
+      <Header />
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
