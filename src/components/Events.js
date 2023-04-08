9006f29336a1d7ffd9a084abb9d6414ad3cfe07f
david-commit <davidondiege@gmail.com>
2023-04-08 19:47:40 +0300

    Fixed: Event slider

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,35 +1,36 @@
 import React from 'react';
 import Slider from 'react-slick';
+import Image from 'next/image';
 
 export default function Events() {
   var settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 4,
     slidesToScroll: 1,
-    arrows: true
+    arrows: true,
   };
+
+  const images = [
+    '/posters/dj-p-k-event-citam.jpg',
+    '/posters/dj-p-k-event-254-live.jpg',
+    '/posters/dj-p-k-heaven-bound-poster.jpg',
+    '/posters/dj-p-k-nrg-show-poster.jpg',
+    '/posters/dj-p-kfrma-nomenee.jpg',
+    '/posters/dj-p-k-xtreem-awards-poster.jpg',
+    '/posters/dj-p-k-marantha-awards-poster.jpg',
+  ];
+
   return (
-    <Slider {...settings} style={{ width: '85%' }}>
-      <div style={{ width: '85%' }}>
-        <h3>1</h3>
-      </div>
-      <div>
-        <h3>2</h3>
-      </div>
-      <div>
-        <h3>3</h3>
-      </div>
-      <div>
-        <h3>4</h3>
-      </div>
-      <div>
-        <h3>5</h3>
-      </div>
-      <div>
-        <h3>6</h3>
-      </div>
+    <Slider {...settings} style={{ width: '85%', margin: 'auto' }}>
+      {images.map((img, i) => {
+        return (
+          <div>
+            <Image src={img} height={100} width={100} alt={`Event poster ${i + 1}`} />
+          </div>
+        );
+      })}
     </Slider>
   );
 }
