fb1e41a6fc5d3c64f458cddfb08b2a12b384d45f
david-commit <davidondiege@gmail.com>
2023-05-22 15:29:02 +0300

    Style: Hire page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,7 @@ import Footer from '@/components/Footer';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Open_Sans } from 'next/font/google'
+import '../styles/Hire.css';
 // import Font Awesome CSS
 import "@fortawesome/fontawesome-svg-core/styles.css"; 
 
--- src/pages/hire.js
+++ src/pages/hire.js
@@ -1,7 +1,63 @@
-import React, { useContext } from 'react';
+import React, { useState } from 'react';
 
 const hire = () => {
-  return <div className='hire-main-container'></div>;
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [eventType, setEventType] = useState('');
+  const [set] = useState('');
+
+  const typesOfEvents = [
+    'Church Event',
+    'Live Show Event',
+    'School Event',
+    'Adult Birthday',
+    'Kids Birthday',
+    'Other',
+  ];
+
+  return (
+    <div className='hire-main-container'>
+      <h1>Hire DJ Prince Kim</h1>
+      <form id='hire-form'>
+        <fieldset>
+          <label htmlFor='name'>
+            Name (Personal or Organisation's){' '}
+            <span className='required'>*</span>
+          </label>
+          <input
+            type='text'
+            id='name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder='Jane Doe'
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor='phone'>
+            Phone <span className='required'>*</span>
+          </label>
+          <input
+            type='text'
+            id='phone'
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            placeholder='(+254) 712 345 678'
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor='event'>
+            Event Type <span className='required'>*</span>
+          </label>
+          <select>
+            <option hidden>--</option>
+            {typesOfEvents.map((type) => (
+              <option>{type}</option>
+            ))}
+          </select>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default hire;
